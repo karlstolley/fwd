cb9e70de4e8d150e79fc4eb1442e1d3b65b07b17
Karl Stolley <karl.stolley@gmail.com>
2016-10-20 21:03:50 -0500

    Linted and improved JavaScript that better handles week hashes

--- site.js
+++ site.js
@@ -7,7 +7,7 @@
       txt = aftertxt;
     }
     this.html(txt);
-  }
+  };
 
   // Utility class for JavaScript detection in CSS
   $('html').addClass('hasjs');
@@ -16,19 +16,24 @@
   // Highlight the current and next weeks
 
   // Get the current time as a Unix timestamp
-  currentTime = new Date();
+  var currentTime = new Date();
   currentTime = Math.round(currentTime.getTime() / 1000);
 
   // Arrays to make reader-friendly days and months
-  namedDays = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday', 'Saturday'];
-  namedMonths = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+  var namedDays = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday', 'Saturday'];
+  var namedMonths = ['January','February','March','April','May','June','July','August','September','October','November','December'];
 
   // Create an array to hold the future weeks
-  var futureWeeks = []
+  var futureWeeks = [];
+
+  // Create a variable to hold HTML for the toggle label
+  var toggledLabel;
+
+
 
   // Go through each week to build the array of future weeks
   $('.week').each(function() {
-      weekTime = new Date($(this).attr('data-expires'));
+      var weekTime = new Date($(this).attr('data-expires'));
       weekTime = Math.round(weekTime.getTime() / 1000);
       if ((weekTime - currentTime) > 0) {
         futureWeeks.push($(this));
@@ -38,26 +43,39 @@
   // Until the semester is over, do nice things with the calendar
   if (futureWeeks.length > 0) {
 
-    // Put an .extended class on all weeks
-    $('.week').each(function() {
-        $(this).addClass('extended');
-    });
+      // Put an .extended class on all weeks
+      $('.week').each(function() {
+          $(this).addClass('extended');
+      });
 
     // Show the current week
     futureWeeks[0].addClass('preview is-current').removeClass('extended').attr('id','current-week');
 
     // If there is another week beyond the current one, show it as well
-    if (futureWeeks[1]!=undefined) {
+    if (futureWeeks[1]!==undefined) {
       futureWeeks[1].addClass('preview is-next').removeClass('extended');
-      var dueDate = new Date(futureWeeks[0].attr('data-expires'))
+      var dueDate = new Date(futureWeeks[0].attr('data-expires'));
       $('#current-week .assigned h3').html('Assigned Work <small>Due ' + namedDays[dueDate.getDay()] + ' ' + namedMonths[dueDate.getMonth()] + ' ' + dueDate.getDate() +'</small>');
     }
 
-  // Toggles for showing full calendar
-  // Append a show link (.button)
-  $('.label').append(' <small class="button"><span class="showing">Current</span> Weeks <a class="toggle" href="#current-week">Show All</a></small>');
+    // Watch for hashes pointing to #current-week or #week-XX
+    if (window.location.hash.indexOf('week') === -1) {
+
+      toggledLabel = ' <small class="button"><span class="showing">Current</span> Weeks <a class="toggle" href="#current-week">Show All</a></small>';
+
+    }
+    else {
+
+      $('.primary').addClass('is-visible');
+      toggledLabel = ' <small class="button"><span class="showing">All</span> Weeks <a class="toggle" href="#current-week">Show Current</a></small>';
+
+    }
 
   }
+
+  // Toggles for showing full calendar
+  // Append a show link (.button)
+  $('.label').append(toggledLabel);
 
   // Toggle to actually show/hide content
   $('.button').on('click', function(e) {
