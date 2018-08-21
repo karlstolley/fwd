ef7779a0b63a57cc98fe00fcea6275fd73ccac12
Karl Stolley <karl.stolley@gmail.com>
2018-08-20 19:59:09 -0500

    Add video button colors and bump SW

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const version = '0.0.1';
+const version = '0.0.2';
 // Detect the service worker's location to set a scope for absolute paths
 const detected_scope = '/'+location.href.split('/')[3]+'/';
 
