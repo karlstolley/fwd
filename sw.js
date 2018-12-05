25f8bccddaa7be1f8ed0ebcb77f0d0809de0a105
Karl Stolley <karl.stolley@gmail.com>
2018-12-05 09:04:21 -0600

    Avoid presenting confusing due-date in final week

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const version = '1.1.1';
+const version = '1.1.2';
 // Detect the service worker's location to set a scope for absolute paths
 const detected_scope = '/'+location.href.split('/')[3]+'/';
 
